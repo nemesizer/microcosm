b6abbda8915e0f4a28c1c19f9febb61c0619c6d1
Ben <coppin@gmail.com>
2022-04-21 13:52:27 +0000

    Added duplicates.py back in

---
+++ duplicates.py
@@ -0,0 +1,26 @@
+from book import BOOK
+# counts how many times each word appears in the book
+
+lines = []
+
+dups = {}
+
+words = {}
+
+for page in BOOK:
+    for line in page:
+        if line in lines:
+            if line not in dups:
+                dups[line] = 0
+            dups[line] += 1
+        else:
+            lines.append(line)
+
+        for word in line.split(' '):
+            if word not in words:
+                words[word] = 0
+            words[word] += 1
+
+print(dups)
+
+print('\n'.join(list(f'{k}: {v}' for k, v in sorted(words.items(), key=lambda item: item[1], reverse=True))))
